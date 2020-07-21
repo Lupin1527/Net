2228711999c8e9e8562dee9c687ed182d2c54a15
zhangxinchen <814638168@qq.com>
2020-07-21 14:41:02 +0800

    real Class

--- test.py
+++ test.py
@@ -1,118 +1,191 @@
-import traceback
-import copy
-
-Anode = {0:0.8,
-         1:0.4,
-         2:0.9}
-
-Bnode = {1:0.7,
-         3:0.3}#-1表示可以被外部攻击，0.8表示被攻击概率为0.8  1表示b可以攻击节点1 概率为0.7
-
-
-Cnode = {3:0.7}
-
-Dnode = {1:0.4,
-         4:0.1}
-
-Enode = {4:0.7}
-
-Fnode = {}
-
-Origin = {-1:1}
-listTest = {
-    -3:Origin,
-    -1: Anode,
-    0: Bnode,
-    1: Cnode,
-    2: Dnode,
-    3: Enode,
-    4: Fnode
-}
-i = 0
-j = 1
-import  stack
-import trace
-print(listTest)
-listTestCopyyyy = copy.deepcopy(listTest)
-attackroad =[]
-test = [['-3',1]]
-stack1 = [(-3,1)]
-coutroad = 0
-
-def NodeToNode(cinlist,node,coutlist,stackbox):
-    try:
-        global coutroad
-        m = node[0]
-        if m!=-2:
-            #print(node)
-            coutlist[coutroad][0]=coutlist[coutroad][0]+','+'%d'%m
-            coutlist[coutroad][1]=coutlist[coutroad][1]*node[1]
+from PyQt5.QtWidgets import QApplication, QWidget
+import sys
+import pyqtgraph as pg
+import numpy as np
+from mainWindow import *
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QIcon, QBrush, QColor
+from PyQt5.QtCore import Qt, pyqtSignal, QPropertyAnimation, QPoint, QAbstractAnimation, QEasingCurve,QLine
+from PyQt5.QtCore import Qt, pyqtSignal
 
 
-        if ((int(m) in cinlist) == True and cinlist.get(m)!={}):
-            stackbox.append(node)
-            n = cinlist.get(m).popitem()
 
-            #coutlist[coutroad]=coutlist[coutroad]+(["%d->%d"%(m,n[0]),node[1]*n[1]])
-            NodeToNode(cinlist,n,coutlist,stackbox)
+from pyqtgraph.Qt import QtCore, QtGui
+#from testForDraw import *
+class Graph(pg.GraphItem):#graph类
+    def __init__(self):
+        self.dragPoint = None
+        self.dragOffset = None
+        self.textItems = []
+        pg.GraphItem.__init__(self)
+        self.scatter.sigClicked.connect(self.clicked)
+
+    def setData(self, **kwds):
+        self.text = kwds.pop('text', [])
+        self.data = kwds
+        if 'pos' in self.data:
+            npts = self.data['pos'].shape[0]
+            self.data['data'] = np.empty(npts, dtype=[('index', int)])
+            self.data['data']['index'] = np.arange(npts)
+        self.setTexts(self.text)
+        self.updateGraph()
+
+    def setTexts(self, text):
+        for i in self.textItems:
+            i.scene().removeItem(i)
+        self.textItems = []
+        for t in text:
+            item = pg.TextItem(t)
+            self.textItems.append(item)
+            item.setParentItem(self)    #这个决定了节点的字
+
+    def updateGraph(self):
+        pg.GraphItem.setData(self, **self.data)
+        for i, item in enumerate(self.textItems):
+            item.setPos(*self.data['pos'][i])
+
+    def mouseDragEvent(self, ev):
+        if ev.button() != QtCore.Qt.LeftButton:
+            ev.ignore()
+            return
+
+        if ev.isStart():
+            # We are already one step into the drag.
+            # Find the point(s) at the mouse cursor when the button was first
+            # pressed:
+            pos = ev.buttonDownPos()
+            pts = self.scatter.pointsAt(pos)
+            if len(pts) == 0:
+                ev.ignore()
+                return
+            self.dragPoint = pts[0]
+            ind = pts[0].data()[0]
+            self.dragOffset = self.data['pos'][ind] - pos
+        elif ev.isFinish():
+            self.dragPoint = None
+            return
         else:
-            if  len(stackbox) == 0 :
-                print('done')
-            else:
-                max1 = len(stackbox)-1
-                stacklast = stackbox[max1][0]
-                coutroad += 1
+            if self.dragPoint is None:
+                ev.ignore()
+                return
 
-                lenname = len(coutlist[coutroad-1][0])
-                coutlist.append(['0',1])
-                temp = coutlist[coutroad-1][0][0:lenname-2]
+        ind = self.dragPoint.data()[0]
+        self.data['pos'][ind] = ev.pos() + self.dragOffset
+        self.updateGraph()
+        ev.accept()
 
-                coutlist[coutroad][0]=temp
-
-                coutlist[coutroad][1]=coutlist[coutroad-1][1]/node[1]
-                timebackNode = cinlist.get(stacklast)
-                #print(timebackNode)
-                if timebackNode!={}:#如果这个不为空，直接读取下一个
-                    #print(timebackNode.popitem())
-                    NodeToNode(cinlist,timebackNode.popitem(),coutlist,stackbox)
-                else:
-                    c = stackbox.pop()#弹出栈顶元素
-                    a = c[0]    #节点好
-                    proba = c[1]    #概率
-                    cinlist[a] = copy.deepcopy(listTestCopyyyy[a]) #每当往回走一步，回朔这个节点所有子节点的访问路径
-                    #print(stack)
-                    b = (-2,proba)
-                    NodeToNode(cinlist,b,coutlist,stackbox)
+    def clicked(self, pts):
+        print("clicked: %s" % pts)
 
 
+class win(QMainWindow,Ui_Form):
+    def __init__(self, parent=None, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.setupUi(self)
 
-#存在问题经过节点会使用掉节点的路径
-#计算数值有问题
-
-
-
-            return 'Done'
-
-    except Exception as result:
-        traceback.print_exc()
+        AddPoint_signal = pyqtSignal()
+        AddLine_signal = pyqtSignal()
 
 
+        test = Graph()
+
+        self.resize(1000,500)
+        self.pw = pg.PlotWidget(self)  # 创建一个绘图控件
+        self.pw.resize(600,400)
+        self.pw.move(20,20)
+        self.pushButton.move(800,200)
+        self.pushButton_2.move(800,250)
+        self.Startlabel.move(808,280)
+        self.startpoint.move(858,285)
+        self.EndLabel.move(808,305)
+        self.EndPoint.move(858,315)
+
+        self.pw.addItem(test)
+        ## Define positions of nodes
+
+        pos = np.array([
+            [0, 0],
+            [10, 0],
+            [0, 10],
+            [10, 10],
+            [5, 5],
+            [15, 5]
+        ], dtype=float)
+        ## Define the set of connections in the graph
+        adj = np.array([
+            [0, 1],
+            [0, 2],
+            [0, 3],
+            [1, 2],
+            [1, 4],
+            [2, 4],
+            [3, 2],
+            [4, 5]
+        ])
+
+        ## Define the symbol to use for each node (this is optional)
+        symbols = ['o', 'o', 'o', 'o', 'o', 'o']
+
+        ## Define the line style for each connection (this is optional)
+        lines = np.array([
+            (255, 0, 0, 255, 22),
+            (255, 0, 255, 255, 2),
+            (255, 0, 255, 255, 3),
+            (255, 255, 0, 255, 2),
+            (255, 0, 0, 255, 1),
+            (255, 255, 255, 255, 4),
+            (255, 255, 255, 255, 4),
+            (255, 255, 255, 255, 4),
+
+        ], dtype=[('red', np.ubyte), ('green', np.ubyte), ('blue', np.ubyte), ('alpha', np.ubyte), ('width', float)])
+        for i in lines:
+            m = i[4]
+            # print('%s'%m)
+            #print(i)
+        ## Define text to show next to each symbol
+        texts = ["Point %d" % i for i in range(6)]
+
+        ## Update the graph
+        test.setData(pos=pos, adj=adj, pen=lines, size=1, symbol=symbols, pxMode=False, text=texts)
+
+        #要将pyqtgraph的图形添加到pyqt5的部件中，我们首先要做的就是将pyqtgraph的绘图方式由window改为widget。PlotWidget方法就是通过widget方法进行绘图的
+         # 在绘图控件中绘制图形
+
+    def AddPoint(self):
+        newpiont = Graph()
+        self.pw.addItem(newpiont)
+        pos = np.array([
+            [1, 1]
+        ], dtype=float)
+
+        ## Define the set of connections in the graph
+
+        ## Define the symbol to use for each node (this is optional)
+        symbols = ['o']
+
+        ## Define the line style for each connection (this is optional)
+        lines = np.array([
+        ], dtype=[('red', np.ubyte), ('green', np.ubyte), ('blue', np.ubyte), ('alpha', np.ubyte), ('width', float)])
+        #for i in lines:
+         #   m = i[1]
+            # print('%s'%m)
+          #  print(i)
+        ## Define text to show next to each symbol
+        texts = ["new point"]
+
+        newpiont.setData(pos=pos,  pen=lines, size=1, symbol=symbols, pxMode=False, text=texts)
+
+        #self.AddPoint_signal.emit()
+
+    def AddLine(self):
+        print(type(self.test))
+
+        #self.AddLine_signal.emit()
 
 
-
-
-
-
-
-
-
-a = (-1,1)
-#print(a)
-
-
-NodeToNode(listTest,a,test,stack1)
-print(test)
-
-
-
-print('test')
+if __name__=='__main__':
+    app=QApplication(sys.argv)
+    w=win()
+    w.show()
+    sys.exit(app.exec_())
